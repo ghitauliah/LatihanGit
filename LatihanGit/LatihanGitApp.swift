282a0beabfc7e14b8fa1a2f5eb1b8125b629fa8a
Ghita <129899294+ghitauliah@users.noreply.github.com>
2023-05-26 10:44:10 +0700

    Initial Commit

---
+++ LatihanGit/LatihanGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  LatihanGitApp.swift
+//  LatihanGit
+//
+//  Created by Ghita Aulia Hidayat on 26/05/23.
+//
+
+import SwiftUI
+
+@main
+struct LatihanGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
